ecc0827cd76ab1dd722faa31bc787a1025a2928a
Abdulrahman_Saad <abdulrahman.saad2303@gmail.com>
2025-08-04 19:53:21 +0300

    finished responsive searchIP and added some functionalitites

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -34,12 +34,20 @@ function Products() {
     fetchData();
   }, []);
 
+  function changeCategory(cat) {
+    setToSearch("");
+    setFilteredProducts([]);
+    setFilteredCategory(cat);
+  }
+
   function onSearch(e) {
     setToSearch(e);
     if (e.length <= 1) return;
     setFilteredProducts(
-      Products.filter((product) =>
-        product.name.toLowerCase().includes(e.toLowerCase())
+      Products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(e.toLowerCase()) &&
+          product.category === filteredCategory
       )
     );
   }
@@ -52,14 +60,14 @@ function Products() {
         <div className="p-4 max-w-6xl mx-auto pb-10">
           <h1 className="text-2xl font-bold mb-4">Products:</h1>
 
-          <div className="flex justify-between items-center mb-6 py-2">
-            <div className="flex flex-wrap ml-5 justify-center items-center gap-4">
+          <div className="justify-center lg:justify-between items-center mb-6 py-2 grid lg:grid-cols-2 grid-cols-1 gap-y-5 ">
+            <div className="flex flex-wrap ml-5 justify-start items-center gap-4">
               <p className="font-semibold text-xl text-gray-600">Sort:</p>
               {categories.map((cat) => (
                 <button
                   key={cat}
                   value={cat}
-                  onClick={() => setFilteredCategory(cat)}
+                  onClick={() => changeCategory(cat)}
                   className={`px-4 py-1.5 transition delay-100 ease-in-out cursor-pointer rounded-full text-gray-600 ${
                     filteredCategory === cat
                       ? "bg-green-700 text-white"
@@ -70,7 +78,7 @@ function Products() {
                 </button>
               ))}
             </div>
-            <div className="w-3/6 flex justify-end">
+            <div className="searchProduct flex justify-center md:justify-end relative">
               <input
                 type="text"
                 name="searchProduct"
@@ -78,8 +86,14 @@ function Products() {
                 value={toSearch}
                 placeholder="Search products..."
                 onChange={(e) => onSearch(e.target.value)}
-                className="search-products w-full max-w-md bg-white border border-gray-300 focus:border-green-700 focus:ring-1 focus:ring-green-600 outline-none px-4 py-2 rounded-full shadow-sm transition duration-300 "
+                className="search-produc-input w-full max-w-md bg-white border border-gray-300 focus:border-green-700 focus:ring-1 focus:ring-green-600 outline-none px-4 py-2 rounded-full shadow-sm transition duration-300 "
               />
+              <button
+                onClick={() => setToSearch("")}
+                className="text-gray-300 hover:text-green-800 transition duration-300"
+              >
+                &times;
+              </button>
             </div>
           </div>
 
